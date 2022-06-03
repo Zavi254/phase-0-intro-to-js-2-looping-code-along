328657710e58a0095322a2350c35b52c25ffb372
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-03 15:08:46 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const cardNames = [];
+
+function writeCards(arrayName,eventName){
+    
+    for(let i = 0; i < arrayName.length; i++){
+        cardNames.push(`Thank you ${arrayName[i]} for the wonderful ${eventName} gift!`);
+    }
+    return cardNames;
+};
+
+writeCards(names,"birthday");
+
+
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
